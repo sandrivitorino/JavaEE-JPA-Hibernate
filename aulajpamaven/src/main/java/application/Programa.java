fcaf1c731f8294ce5173210dbfe38c5b8059b88a
vonvit <sandrineviturn@gmail.com>
2026-05-10 20:26:34 -0300

    Finalizando o mini projeto sobre Nivelamento JPA/Hibernate

--- aulajpamaven/src/main/java/application/Programa.java
+++ aulajpamaven/src/main/java/application/Programa.java
@@ -1,17 +1,26 @@
 package application;
 
+
 import dominio.Pessoa;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 	}
 
 }
